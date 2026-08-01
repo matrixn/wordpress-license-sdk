c5b8e7180c52d2f14b4f1d549eafffaf65a90061
Alex Radu Raduta <raduta.alex@gmail.com>
2026-08-02 02:46:19 +0300

    Ridica

--- src/LicenseManager.php
+++ src/LicenseManager.php
@@ -6,7 +6,7 @@ use Zion\WordPressLicense\Exceptions\ApiException;
 
 final class LicenseManager
 {
-    public const VERSION = '0.3.0';
+    public const VERSION = '0.4.0';
 
     private ?LicensePrompt $prompt = null;
 
